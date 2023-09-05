a3d707f2e874ce758dd69047c261dd06a3193121
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-09-05 14:19:53 +0500

    postgres connection setup using typeOrm

--- Nest_API/src/app.module.ts
+++ Nest_API/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": "localhost",
+      "port": 5433,
+      "username": "postgres",
+      "password": "fast",
+      "database": "multidisease_db",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
