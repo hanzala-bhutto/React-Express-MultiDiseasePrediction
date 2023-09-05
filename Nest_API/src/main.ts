84afa1337b81b960ce3f3e482847417361b7ce12
hanzala-bhutto <bhutto.hanzala@gmail.com>
2023-09-05 12:15:48 +0500

    setup swagger

--- Nest_API/src/main.ts
+++ Nest_API/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+  const config = new DocumentBuilder()
+  .setTitle('Multi Disease API')
+  .setDescription('The Multi Disease API description')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(3001);
 }
 bootstrap();
